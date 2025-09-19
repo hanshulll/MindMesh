e20183df9229aeab0e90db7d8ac7fbdb7da6e8eb
Hanshul Chandel <hanshulchandel2001@gmail.com>
2025-09-19 23:11:16 +0530

    Implemented API to create user and added dependency for spring-security + modelMapper
    Changelog: addeD

---
+++ src/main/java/com/hanshul/mindmesh/config/BeanFactory/ModelMapperFactory.java
@@ -0,0 +1,13 @@
+package com.hanshul.mindmesh.config.BeanFactory;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperFactory {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
--- src/main/java/com/hanshul/mindmesh/user/model/dto/response/UserResponseDto.java
+++ src/main/java/com/hanshul/mindmesh/user/model/dto/response/UserResponseDto.java
@@ -1,10 +1,12 @@
 package com.hanshul.mindmesh.user.model.dto.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class UserResponseDto {
     private String firstName;
     private String secondName;
